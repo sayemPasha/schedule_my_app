2a379664d18a3a0fb25f59fac1bb8f9a4a86eee3
Sayem Pasha <sayem.pasha@bs-23.net>
2025-03-03 15:31:11 +0600

    feat: date dialog updated

--- app/src/main/java/com/sayem/main/ui/scheduler/ScheduleDetailScreen.kt
+++ app/src/main/java/com/sayem/main/ui/scheduler/ScheduleDetailScreen.kt
@@ -13,11 +13,18 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,6 +34,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import java.util.Calendar
@@ -57,6 +66,7 @@ fun ScheduleDetailRoute(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScheduleDetailScreen(
     uiState: ScheduleDetailUiState,
@@ -152,16 +162,63 @@ fun ScheduleDetailScreen(
     }
 
     if (showTimePicker) {
-        val calendar = Calendar.getInstance()
-        TimePickerDialog(
-            context,
-            { _, hour, minute ->
-                onUpdateTime(hour, minute)
-                showTimePicker = false
-            },
-            calendar.get(Calendar.HOUR_OF_DAY),
-            calendar.get(Calendar.MINUTE),
-            false
-        ).show()
+        val currentTime = Calendar.getInstance()
+//        TimePickerDialog(
+//            context,
+//            { _, hour, minute ->
+//                onUpdateTime(hour, minute)
+//                showTimePicker = false
+//            },
+//            calendar.get(Calendar.HOUR_OF_DAY),
+//            calendar.get(Calendar.MINUTE),
+//            false
+//        ).show()
+
+        Dialog(
+            properties = DialogProperties(usePlatformDefaultWidth = false),
+            onDismissRequest = {}
+        ){
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+
+                colors = CardDefaults.cardColors(
+                    contentColor = MaterialTheme.colorScheme.onSurface,
+                    containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                )
+            ){
+                Column(modifier = Modifier
+                    .padding(horizontal = 20.dp, vertical = 40.dp)
+                    .fillMaxWidth()){
+                    val timePickerState = rememberTimePickerState(
+                        initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
+                        initialMinute = currentTime.get(Calendar.MINUTE),
+                        is24Hour = false,
+                    )
+
+                    TimePicker(
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        state = timePickerState,
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        OutlinedButton (onClick = { showTimePicker = false }) {
+                            Text("Cancel")
+                        }
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(onClick = {
+                            onUpdateTime(timePickerState.hour, timePickerState.minute)
+                            showTimePicker = false
+                        }) {
+                            Text("Confirm selection")
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
